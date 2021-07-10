ca51a58e67f719c76f8f0e68ccea02dc90406b45
João Guilherme Jarochinski Marinho <joaogui3856@hotmail.com>
2021-07-10 19:03:19 -0300

    Undoing bad things

--- src/main.c
+++ src/main.c
@@ -18,48 +18,18 @@ int main(void)
     case 10:
         generateIndexLines();
         break;
-        /*
-        Entrada do programa para a funcionalidade [9] e [10]:
-
-        9 veiculo.bin indicePrefixo.bin
-        ou
-        10 linha.bin indiceCodLinha.bin
-        */
-
-
     case 11:
         searchIndexedCars();
         break;
     case 12:
         searchIndexedLines();
         break;
-        /*
-        Sintaxe do comando para a funcionalidade [11] e [12]: 
-
-        11 veiculo.bin indicePrefixo.bin prefixo "valor"
-        ou
-        12 linha.bin indiceCodLinha.bin codLinha valor
-        */
-
-
     case 13:
+        insertAndIndexCars();
         break;
     case 14:
+        insertAndIndexLines();
         break;
-        /*
-        Entrada do programa para a funcionalidade [13] e [14]: 
-
-        13 veiculo.bin indicePrefixo.bin n 
-        prefixo1 data1 quantidadeLugares1 codLinha1 modelo1 categoria1
-        prefixo2 data2 quantidadeLugares2 codLinha2 modelo2 categoria2
-        ... 
-        ou
-        14 arquivoLinha.bin indiceCodLinha.bin n 
-        codLinha1 aceitaCartao1 nomeLinha1 corLinha1
-        codLinha2 aceitaCartao2 nomeLinha2 corLinha2
-        ... 
-        codLinhan aceitaCartaon nomeLinhan corLinhan
-        */
     }
 
     return EXIT_SUCCESS;
